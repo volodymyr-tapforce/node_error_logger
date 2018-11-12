b5300c96dea391c8aff05c1c295533461db9ad7a
volodymyr <volodymyr@tapforce.com>
2018-11-12 17:09:08 +0200

    serve index.html from react build like homepage

--- app.js
+++ app.js
@@ -23,11 +23,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
+app.use('/errorlist', indexRouter);
+app.use('/userlist', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/errors', errorRouter);
+// app.use('/*', indexRouter);
+
 
 
 // catch 404 and forward to error handler
