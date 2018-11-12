0c578307972cd4db123ced1b016f643ba090db95
volodymyr <volodymyr@tapforce.com>
2018-11-12 14:28:37 +0200

    submit create error to server

--- client/src/components/errorCreateForm.js
+++ client/src/components/errorCreateForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import ErrorFormInput from './errorFormInput';
+import axios from 'axios';
 
 class ErrorForm extends Component {
 
@@ -17,11 +18,35 @@ class ErrorForm extends Component {
         newState[event.target.name] = event.target.value;
         this.setState(newState);
     }
+
+    handleSubmit = ()=>{
+        const componentRef = this;
+        axios.post('/errors', {
+            userParams:{
+                anonymous_id:this.state.anonymous_id
+            },
+            err_type: this.state.err_type,
+            err_message:this.state.err_message
+          })
+          .then(function (response) {
+            if(response.status===200){
+                componentRef.setState({
+                    anonymous_id:'',
+                    err_type:'',
+                    err_message:''
+                })
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
     render() {
       return (
         <div style={{width:'100%'}}>
         <Segment >
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <ErrorFormInput name={'anonymous_id'} value={this.state.anonymous_id} handleChange={this.handleChange}></ErrorFormInput>
           <ErrorFormInput name={'err_type'} value={this.state.err_type}  handleChange={this.handleChange}></ErrorFormInput>
           <ErrorFormInput name={'err_message'} value={this.state.err_message}  handleChange={this.handleChange}></ErrorFormInput>
