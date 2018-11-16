9998b155b307b5c6329be7560500a67a8bd4bb66
volodymyr <volodymyr@tapforce.com>
2018-11-16 17:35:30 +0200

    update user

--- controllers/postgresql/users.js
+++ controllers/postgresql/users.js
@@ -1,18 +1,29 @@
 const userModel = require('../../models/postgresql/users');
 
 const userController = {
-    findUserByAnonId: (userParams, findCallback) => {
-        userModel.findOne({ where: {anonymous_id: userParams.anonymous_id} }).then(user => {
-            findCallback(user);
+    findUserByAnonId:  (userParams, findCallback) => {
+        userModel.findOne({ where: {anonymous_id: userParams.anonymous_id} }).then(async (user )=> {
+            if(user === null){
+                const newUser = await userController.createUser(userParams);
+                findCallback(newUser);
+            } else {
+                const updatedUser = await userController.updateUser(userParams, user);
+                findCallback(updatedUser);
+            }
         })
-        // userController.createUser(userParams,result=>{
-        //     findCallback(result); 
-        // })
     },
-    createUser:(userParams, createCallback)=>{
-        userModel.create(userParams).then(createCallback);
-        // newUser.save(createCallback);
-    }
+    createUser:(userParams)=>{
+        return userModel.create(userParams);
+    },
+    updateUser:(userParams, user)=>{
+        const paramsForUpdate = {
+            lastErrorTime: new Date()
+        }
+        if(userParams.email&&(user.email!==userParams.email)) paramsForUpdate.email = userParams.email;
+        if(userParams.user_id&&(user.user_id!==userParams.user_id)) paramsForUpdate.user_id = userParams.user_id;
+
+        return user.update(paramsForUpdate);
+    }, 
 
 }
 
