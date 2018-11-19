911168446c5c55858fa05409184f047f940af7bf
volodymyr <volodymyr@tapforce.com>
2018-11-19 11:42:37 +0200

    full migrate mongoose controller to sequlize.

--- controllers/postgresql/errors.js
+++ controllers/postgresql/errors.js
@@ -2,17 +2,22 @@ const errorModel = require('../../models/postgresql/errors');
 const userController = require('./users');
 
 const errorController = {
-    createError:(req, res, next)=>{
-        // errorModel.create({
-        //     anonymous_id:'1111',
-        //     err_type:'warning',
-        //     err_message:'12321312312'
-        // }).then((result)=>{
-        //     res.send(result);
-        // })
-        userController.findUserByAnonId(req.body.userParams, (user)=>{
+    createError: (req, res, next)=>{
+  
+        userController.findUserByAnonId(req.body.userParams, async (user)=>{
             // if empy return null
-            res.send(user);
+            if(user) {
+                const errorDocParams = {
+                    anonymous_id: user.anonymous_id,
+                    err_type: req.body.err_type,
+                    err_message: req.body.err_message,
+                }
+
+                const newError = await errorModel.create(errorDocParams);
+                return res.send({isNewUser:true, anonymous_id:user.anonymous_id});
+            } else{
+                return next(new Error('error create problem'));
+            }
         });
     },
     getErrorByAnonId:async (req, res, next)=>{
@@ -24,7 +29,7 @@ const errorController = {
         const limit = req.query.limit||10;
 
         const errorDocs = await errorModel.findAll({ where,limit, offset, order: [['createdAt', 'DESC']]  });
-        res.send({errorsCount,errorDocs});
+        return res.send({errorsCount,errorDocs});
     }
 }
 
