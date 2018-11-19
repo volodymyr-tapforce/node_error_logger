537499898b5a139778fee1e4ec4e1ed0b222c74a
volodymyr <volodymyr@tapforce.com>
2018-11-19 15:42:16 +0200

    heroku postgress

--- db/sequelizejsConfig.js
+++ db/sequelizejsConfig.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('test', 'postgres', 'webdev`8', {
-    host: 'localhost',
+// const sequelize = new Sequelize('test', 'postgres', 'webdev`8', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     operatorsAliases: false,
+// });
+
+const sequelize = new Sequelize('d3irhto7nrvn23', 'qflavxhbvejvod', '2724ac590e01525e8274feb9f9020f8c15576c4c6b7a4f91a1a14891742b505b', {
+    host: 'ec2-54-246-85-234.eu-west-1.compute.amazonaws.com',
     dialect: 'postgres',
     operatorsAliases: false,
 });
