fa7b4801b983dbb24aa38665c6b159b1f0fc7eec
volodymyr <volodymyr@tapforce.com>
2018-11-23 15:50:01 +0200

    Added pagination query

--- models/graphqlShemas/errors.js
+++ models/graphqlShemas/errors.js
@@ -9,8 +9,21 @@ export default gql`
     createdAt: Date!
   }
 
+  type ErrorConnection {
+    edges: [Error!]!
+    pageInfo: PageInfo!
+  }
+
+  type PageInfo {
+    hasNextPage: Boolean!
+    endCursor: String!
+  }
+
   extend type Mutation {
     createError(anonymusId: String!,err_type: String!,err_message: String!): Error!
   }
 
+  extend type Query{
+    errors (cursor: String, limit: Int): ErrorConnection!
+  }
 `;
--- qraphqlResolvers/errors.js
+++ qraphqlResolvers/errors.js
@@ -1,5 +1,43 @@
 import models from '../models/sequelize';
+import Sequelize from 'sequelize';
+
+const toCursorHash = string => Buffer.from(string).toString('base64');
+const fromCursorHash = string =>
+  Buffer.from(string, 'base64').toString('ascii');
+
 export default {
+    Query:{
+        errors:async (parent, { cursor, limit = 100 }) => {
+            const cursorOptions = cursor
+              ? {
+                  where: {
+                    createdAt: {
+                      [Sequelize.Op.lt]: fromCursorHash(cursor),
+                    },
+                  },
+                }
+              : {};
+      
+            const errors = await models.Error.findAll({
+              order: [['createdAt', 'DESC']],
+              limit: limit + 1,
+              ...cursorOptions,
+            });
+      
+            const hasNextPage = errors.length > limit;
+            const edges = hasNextPage ? errors.slice(0, -1) : errors;
+      
+            return {
+              edges,
+              pageInfo: {
+                hasNextPage,
+                endCursor: toCursorHash(
+                  edges[edges.length - 1].createdAt.toString(),
+                ),
+              },
+            };
+        }
+    },
     Mutation:{
         createError:  async (parent, { anonymusId, err_type, err_message }) => {
             const error = await models.Error.create({
